d52155bb2ba8a26f793c704d61c03085e9aff015
fullsunncit <nurrosyd13@gmail.com>
2023-03-16 13:56:10 +0700

    javascript day 2

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-function bookPurchasing({title, price, category, author, status}, discount, tax) {
-    const amountOfDiscount = price * (discount / 100);
-    const priceAfterDiscount = price - amountOfDiscount;
-    const amountOfTax = priceAfterDiscount * (tax / 100);
-    const priceAfterTax = priceAfterDiscount + amountOfTax;
-    
-    return {
-        title: title,
-        price: price,
-        category: category,
-        author: author,
-        status : status,
-        amountOfDiscount: amountOfDiscount,
-        priceAfterDiscount: priceAfterDiscount,
-        amountOfTax: amountOfTax,
-        priceAfterTax: priceAfterTax
-    };
+function bookPurchasing({title, price, category, author, status, stock}, discount, tax, amountPurchased) {
+    let jumlah = 0;
+    for (let i=1; i<=amountPurchased; i++){
+        if(amountPurchased <= stock){
+            const amountOfDiscount = price * (discount / 100);
+            const priceAfterDiscount = price - amountOfDiscount;
+            const amountOfTax = priceAfterDiscount * (tax / 100);
+            const priceAfterTax = priceAfterDiscount + amountOfTax;
+            console.log("Buku ke-" + i);
+            console.log("- Title : " + title);
+            console.log("- Price : " + price);
+            console.log("- Category : " + category);
+            console.log("- Author : " + author);
+            console.log("- Price : " + price);
+            console.log("- Amount of discount : " + amountOfDiscount);
+            console.log("- Price after discount : " + priceAfterDiscount);
+            console.log("- Amount of tax : " + amountOfTax);
+            console.log("- Price after tax and discount : " + priceAfterTax);
+            jumlah = jumlah + priceAfterTax;
+        }
+        else{
+            console.log("Maaf, stok buku tidak memenuhi");
+        }
+    }
+    console.log(" ");
+    console.log("==> Total harga semua buku : " + jumlah);
 }
         
 let book = {
@@ -22,8 +31,8 @@ let book = {
     "price": 100000,
     "category" : "Novel",
     "author" : "Tenderlova",
-    "status" : true
+    "status" : true,
+    "stock" : 10
 }
 
-
-console.log(bookPurchasing(book, 20, 5));
+bookPurchasing(book, 20, 5, 3);
